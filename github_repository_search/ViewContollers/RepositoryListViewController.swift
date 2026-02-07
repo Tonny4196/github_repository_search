e88f6bb8db71ce803e95ee1b2dad2d50b5a2baef
Tonny4196 <tomohiro4196@gmail.com>
2026-02-07 14:26:23 +0900

    起動画面の設定

---
+++ github_repository_search/ViewContollers/RepositoryListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RepositoryListViewController.swift
+//  github_repository_search
+//
+//  Created by Tomohiro Takahashi on 2026/02/07.
+//
+
+import UIKit
+
+class RepositoryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
